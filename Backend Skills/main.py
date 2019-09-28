f1d4b05ae743194dd4ddcd6f31cdb8d6906a2a50
Tamires Santana <tamesps@gmail.com>
2019-09-28 17:22:39 -0300

    Adiciona resolução do desafio de backend

---
+++ Backend Skills/main.py
@@ -0,0 +1,11 @@
+import jwt
+
+def create_token(data, secret):
+	return jwt.encode(data, secret, algorithm='HS256')
+
+def verify_signature(token):
+	try:
+		return jwt.decode(token, 'acelera', algorithms='HS256')
+	except jwt.ExpiredSignatureError:
+		return {"error": 2}
+
