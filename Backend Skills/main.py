f1d60e9468849e0d69cb035861665228479ecc6f
Tamires <tamesps@gmail.com>
2019-11-16 00:08:22 -0300

    Modifica estilo do código

--- Backend Skills/main.py
+++ Backend Skills/main.py
@@ -1,11 +1,12 @@
 import jwt
 
+
 def create_token(data, secret):
-	return jwt.encode(data, secret, algorithm='HS256')
+    return jwt.encode(data, secret, algorithm='HS256')
+
 
 def verify_signature(token):
-	try:
-		return jwt.decode(token, 'acelera', algorithms='HS256')
-	except jwt.ExpiredSignatureError:
-		return {"error": 2}
-
+    try:
+        return jwt.decode(token, 'acelera', algorithms='HS256')
+    except jwt.ExpiredSignatureError:
+        return {"error": 2}
--- Data Design/models.py
+++ Data Design/models.py
@@ -1,15 +1,39 @@
+import ipaddress
 from django.db import models
 import datetime
+from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
+from django.utils.translation import gettext_lazy as _
+
+
+def validate_email(string):
+    validator = EmailValidator()
+    try:
+        validator(string)
+    except ValidationError:
+        return False
+    return True
+
+
+def validate_ipv4_address(value):
+    try:
+        ipaddress.IPv4Address(value)
+    except ValueError:
+        raise ValidationError(_('Enter a valid IPv4 address.'), code='invalid')
+
+
+def validate_level(value):
+    if value not in ['CRITICAL', 'DEBUG', 'ERROR', 'WARNING', 'INFO']:
+        raise ValidationError("Invalid value.")
 
 
 class User(models.Model):
     name = models.CharField('Name', max_length=50)
     last_login = models.DateTimeField(auto_now_add=True)
-    email = models.CharField('Email', max_length=254)
-    password = models.CharField('Password', max_length=50)
-    #raise NotImplementedError
-    def __str__(self):
-        return f'{self.name}'
+    email = models.CharField('Email', max_length=254,
+                             validators=[validate_email])
+    password = models.CharField(
+        'Password', max_length=50, validators=[MinValueValidator(8)])
 
 
 class Agent(models.Model):
@@ -17,22 +41,28 @@ class Agent(models.Model):
     status = models.BooleanField()
     env = models.CharField('env', max_length=20)
     version = models.CharField(max_length=5)
-    address = models.TextField('address', default='', max_length=39)
+    address = models.TextField('address', default='', max_length=39, validators=[
+                               validate_ipv4_address])
 
-    
+
 class Event(models.Model):
-    level = models.CharField('level', max_length=20)
+    level = models.CharField('level', max_length=20,
+                             validators=[validate_level])
     data = models.TextField('data', default='', max_length=39)
     arquivado = models.BooleanField()
     date = models.DateTimeField(auto_now_add=True)
-    agent = models.ForeignKey(Agent, on_delete=models.deletion.DO_NOTHING, default='')
-    user = models.ForeignKey(User, on_delete=models.deletion.DO_NOTHING,default='')
+    agent = models.ForeignKey(
+        Agent, on_delete=models.deletion.DO_NOTHING, default='')
+    user = models.ForeignKey(
+        User, on_delete=models.deletion.DO_NOTHING, default='')
 
 
 class Group(models.Model):
     name = models.CharField('Name', max_length=50)
 
-    
+
 class GroupUser(models.Model):
-    group = models.ForeignKey(Group, on_delete=models.deletion.DO_NOTHING,default='')
-    user = models.ForeignKey(User, on_delete=models.deletion.DO_NOTHING,default='')
+    group = models.ForeignKey(
+        Group, on_delete=models.deletion.DO_NOTHING, default='')
+    user = models.ForeignKey(
+        User, on_delete=models.deletion.DO_NOTHING, default='')
